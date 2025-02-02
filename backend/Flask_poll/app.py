7343e06acb7f2a9c8d4386d0884bf5fc9455f400
Софія Федоренко <ssddf_345@Noutbuk-Sofia.local>
2025-02-02 13:35:12 +0200

    Adding changes - lesson 2.02.2025

---
+++ backend/Flask_poll/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import os
+
+
+app = Flask(__name__)
+
+poll_data = {
+    'question': 'Which framework do you use?',
+    'fields': ('Flask', 'Django')
+}
+
+filename = 'data.txt'
+
+@app.route('/')
+def root():
+    return render_template('poll.html', data=poll_data)
+
+
+@app.route('/poll')
+def poll():
+    vote = request.args.get('field')
+    out = open(filename, 'a')
+    out.write(vote + '\n')
+    out.close()
+    return vote
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
